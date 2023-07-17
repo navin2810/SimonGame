79d662774bcc7876192cd6c30c390066b1e8e8d0
Navinkumar Balla <118623606+navin2810@users.noreply.github.com>
2023-07-17 11:17:50 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,82 @@
+let buttonColors = ["red", "blue", "green", "yellow"];
+let gamePattern = [];
+let userClickedPattern = [];
+
+let started = false;
+let level = 0;
+
+$(".start").click(function () {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+    $(".start").hide()
+  }
+});
+
+$(".btn").click(function () {
+  let userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    // console.log("success");
+    if (gamePattern.length === userClickedPattern.length) {
+      setTimeout(function () {
+        nextSequence();
+        
+      }, 1000);
+    }
+  }
+  else {
+    // console.log("wrong");
+    playSound("wrong");
+    $("body").addClass("game-over");
+    $("#level-title").text("Game Over, Press Start to Restart");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    
+    startOver();
+  }
+}
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level "+ level)
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+}
+
+
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function playSound(name) {
+  let audio = new Audio("sounds/"+name+".mp3");
+  audio.play();
+}
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+  $(".start").show();
+  
+}
+
+
